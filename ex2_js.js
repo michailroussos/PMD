dd0686a2ec45b4e8a87f869ca649ac9233f3c84a
michailroussos <michael_roussos@yahoo.com>
2020-05-17 17:04:05 +0300

    slide show try

--- ex2_js.js
+++ ex2_js.js
@@ -1,13 +1,66 @@
 window.onload = function(){
 	document.getElementById("Register").addEventListener("click", search);
-	
+
 	suggestions("suggestions","Batman Returns");
+	suggestions("suggestions","Sherlock Holmes: A Game of Shadows");
+	suggestions("suggestions","John Wick");
+	suggestions("suggestions","The Matrix");
+	
+	var slideIndex = 1;
+	showSlides(slideIndex);
+
 	document.getElementById("Search_button").addEventListener("click", show_search_bar);
 	window.addEventListener("resize", search_bar_toggle);
+	/*window.addEventListener("resize", adjust_suggestions);*/
 };
 
 var link='http://www.omdbapi.com/?apikey=ebcdb9f6';
 
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+  
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+	console.log(slideIndex);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("myPosters");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {slideIndex = 1}    
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex-1].style.display = "block";  
+	dots[slideIndex-1].className += " active";
+  }
+
+function adjust_suggestions(){
+	document.getElementById("suggestions").innerHTML='';
+	if(window.innerWidth > 992 ){
+		suggestions("suggestions","Batman Returns");
+		suggestions("suggestions","Sherlock Holmes: A Game of Shadows");
+		suggestions("suggestions","John Wick");
+		suggestions("suggestions","The Matrix");
+	}else if(window.innerWidth > 600 ){
+		suggestions("suggestions","Batman Returns");
+		suggestions("suggestions","Sherlock Holmes: A Game of Shadows");
+		suggestions("suggestions","John Wick");
+	}else if(window.innerWidth > 300 ){
+		suggestions("suggestions","Batman Returns");
+		suggestions("suggestions","John Wick");
+	}else{
+		suggestions("suggestions","Batman Returns");
+
+	}
+}
 function search_bar_toggle(){
 	if(window.innerWidth< 992 ){
 		document.getElementById("Search_button").style.visibility = "visible";
@@ -75,7 +128,7 @@ function specific_search(title){
 
 	request.send();
 }
-function suggestions(id,title){
+function suggestions(id,title,position,total_posters){
 	
 	var request = new XMLHttpRequest();
 	var movielink=link.concat('&t=',title);
@@ -87,8 +140,15 @@ function suggestions(id,title){
 		if (request.status >= 200 && request.status < 400) {
 			var url=document.createElement("A");
 			url.href="https://www.w3schools.com/html/html_links.asp";
+
+			var numbertext=document.createElement("DIV");
+			numbertext.innerText=position.concat("/",total_posters);
+			numbertext.classList.add("numbertext");
 			var poster = document.createElement("DIV");
+			poster.appendChild(numbertext);
 			var img = document.createElement("IMG");
+			img.style.width ='75%' ;
+			img.style.height ='80%' ;
 			img.class="poster";
 			
 			img.src=data.Poster;
@@ -104,8 +164,11 @@ function suggestions(id,title){
 			poster.appendChild(url1);
 			var info = document.createElement("DIV");
 			info.innerText ='('.concat(data.Year,') ',data.Plot,'\n');
-			info.style.width ='30%' ;
+			info.style.width ='75%' ;
 			poster.appendChild(info);
+			poster.classList.add("myPosters");
+			poster.classList.add("fade");
+			
 			document.getElementById(id).appendChild(poster);
 		} else {
 			console.log('error');
